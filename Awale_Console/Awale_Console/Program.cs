4a07b6f7bd6842575fe830794a6525efd9553869
Eddine Farid OMAR <eddine.farid@hotmail.fr>
2017-07-15 07:43:50 +0100

    Adding a static method called Start()

--- Awale_Console/Awale_Console/Program.cs
+++ Awale_Console/Awale_Console/Program.cs
@@ -10,34 +10,7 @@ namespace Awale_Console
 	{
 		public static void Main (string[] args)
 		{
-            ConsoleKey keyPressed;
-            StartGame Run = new StartGame();
-
-            do
-            {
-                Console.Clear();
-                Console.WriteLine("Which game do you want to start ?");
-                Console.WriteLine("[1] Player vs Player");
-                Console.WriteLine("[2] Player vs Computer");
-                Console.WriteLine("[3] Player vs Remote Player");
-                //Console.WriteLine("Make your choice with the keyboard :");
-                keyPressed = Console.ReadKey().Key;
-            } while(keyPressed != ConsoleKey.D1 && keyPressed != ConsoleKey.D2 && keyPressed != ConsoleKey.D3);
-            
-            switch(keyPressed)
-            {
-                case ConsoleKey.D1:
-                    Run.PlayerVsPlayer();    
-                    break;
-
-                case ConsoleKey.D2:
-                    Run.PlayerVsComputer();    
-                    break;
-
-                case ConsoleKey.D3:
-                    Run.PlayerVsRemotePlayer();    
-                    break;
-            }
+            StartGame.Start();
 		}
 
 	}
--- Awale_Console/Awale_Console/StartGame.cs
+++ Awale_Console/Awale_Console/StartGame.cs
@@ -179,6 +179,38 @@ namespace Awale_Console
             Console.ReadLine();
 
         }
+    
+        static public void Start()
+        {
+            ConsoleKey keyPressed;
+            StartGame Run = new StartGame();
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Which game do you want to start ?");
+                Console.WriteLine("[1] Player vs Player");
+                Console.WriteLine("[2] Player vs Computer");
+                Console.WriteLine("[3] Player vs Remote Player");
+                //Console.WriteLine("Make your choice with the keyboard :");
+                keyPressed = Console.ReadKey().Key;
+            } while(keyPressed != ConsoleKey.D1 && keyPressed != ConsoleKey.D2 && keyPressed != ConsoleKey.D3);
+
+            switch(keyPressed)
+            {
+                case ConsoleKey.D1:
+                    Run.PlayerVsPlayer();    
+                    break;
+
+                case ConsoleKey.D2:
+                    Run.PlayerVsComputer();    
+                    break;
+
+                case ConsoleKey.D3:
+                    Run.PlayerVsRemotePlayer();    
+                    break;
+            }
+        }
     }
 }
 
