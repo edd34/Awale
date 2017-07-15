1dd138c1b9571ef3205d222ffbdd799229a6f8d6
Eddine Farid OMAR <eddine.farid@hotmail.fr>
2017-07-15 07:15:17 +0100

    Network features are implemented, some debug to do

--- Awale_Console/Awale_Console/Program.cs
+++ Awale_Console/Awale_Console/Program.cs
@@ -10,13 +10,34 @@ namespace Awale_Console
 	{
 		public static void Main (string[] args)
 		{
+            ConsoleKey keyPressed;
             StartGame Run = new StartGame();
-            //Run.PlayerVsPlayer();
-            //Run.PlayerVsComputer();
-            Run.PlayerVsRemotePlayer();
-            Console.ReadLine();
 
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Which game do you want to start ?");
+                Console.WriteLine("[1] Player vs Player");
+                Console.WriteLine("[2] Player vs Computer");
+                Console.WriteLine("[3] Player vs Remote Player");
+                //Console.WriteLine("Make your choice with the keyboard :");
+                keyPressed = Console.ReadKey().Key;
+            } while(keyPressed != ConsoleKey.D1 && keyPressed != ConsoleKey.D2 && keyPressed != ConsoleKey.D3);
+            
+            switch(keyPressed)
+            {
+                case ConsoleKey.D1:
+                    Run.PlayerVsPlayer();    
+                    break;
 
+                case ConsoleKey.D2:
+                    Run.PlayerVsComputer();    
+                    break;
+
+                case ConsoleKey.D3:
+                    Run.PlayerVsRemotePlayer();    
+                    break;
+            }
 		}
 
 	}
